4a85b3f79f5370f5deb9837294cf283a1e7e6277
F Schnieber <Hitrag_mc@outlook.com>
2025-03-26 19:03:15 +0100

    any comment here in quotes

--- NumberGuesingGame/NumberGuesingGame.cpp
+++ NumberGuesingGame/NumberGuesingGame.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <random>
 #include <stdio.h>
-// Last Worked on: 19:00 26/03/2025
+// Last Worked on: 19:00 2025/03/26
 // What does it do: asks you for what diffuculty to play it
 // Making it shorter via functions
 int anscheck(int r, int g) {
